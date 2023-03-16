eb4727d95d67ce723ad265fbb13fb3f7e0e93e66
nadim1212 <nadimniloy.swe@gmail.com>
2023-03-16 14:55:04 +0600

    global variables

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 import { Provider } from 'react-redux';
 import store from '@/store';
 import { PersistGate } from 'redux-persist/integration/react';
